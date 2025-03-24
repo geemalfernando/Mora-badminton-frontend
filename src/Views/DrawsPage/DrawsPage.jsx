3d03ad9a8c24d7c0a4775170ee0327ed84203844
Hevindu <95610343+Hevb890@users.noreply.github.com>
2025-03-24 14:44:41 +0530

    Update DrawsPage.jsx

--- src/Views/DrawsPage/DrawsPage.jsx
+++ src/Views/DrawsPage/DrawsPage.jsx
@@ -137,7 +137,7 @@ const DrawsPage = () => {
   const draws = [
     {
       name: "Age Group",
-      pdf: "https://docs.google.com/spreadsheets/d/1ESkJYwWKTjqHiN7NcvLrQIETL3CdlTyn/edit?usp=sharing&ouid=108516483365954862566&rtpof=true&sd=true",
+      pdf: "https://docs.google.com/spreadsheets/d/1McZ_9bKe7DqFSqr4J1jrPRyPaBbo8DTP/edit?usp=sharing&ouid=108516483365954862566&rtpof=true&sd=true",
     },
     {
       name: "University Team",
